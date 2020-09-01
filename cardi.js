df658fe95ae9602d8f6a235767fcd66768a5e3cb
Dennis Mwangi <rieratech@gmail.com>
2020-09-01 18:25:11 +0300

    Fixed several bugs: firstPlay didnt splice first play card from deck game issued card to punishing player instead of next player spelling mistakes PL instead of Pl match did not match punisher cards even after player was punished

--- cardi.js
+++ cardi.js
@@ -11,7 +11,7 @@ class Cards{
     constructor(){
         this.ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
         this.suits = ["DIA", "FLW", "HRT", "SPD"],
-        this.noneStarters = ["A", "2", "3", "J" , "Q", "K"],
+        this.noneStarters = ["A", "2", "3", "8", "J" , "Q", "K"],
         this.questionRanks = ["8", "Q"],
         this.noneAnswers = ["2", "3", "J", "K"],
         this.punisherRanks = ["2", "3"],
@@ -45,24 +45,21 @@ class Cards{
         let randomPosition;
         while(!cardMatched){
             randomPosition = Math.floor(Math.random() * (this.deck.length - 1));
-            if(!this.isInGroup(this.noneStarters, this.deck[randomPosition].rank)) cardMatched = true;
+            cardMatched = !this.isInGroup(this.noneStarters, this.deck[randomPosition].rank);
         }
         this.table.push(this.deck[randomPosition]);
-        this.deck.splice(this.deck[randomPosition], 1);
+        this.deck.splice(randomPosition, 1);
         return this;
     }
-    match(attempt, liveCard, continuousPlay = false){
-        if(liveCard.rank === "X"){
-            return attempt.suit === liveCard.suit
-        };
+    match(attempt, liveCard, continuousPlay = false, punish = false){
+        if(liveCard.rank === "X") return attempt.suit === liveCard.suit;
         if(attempt.rank === liveCard.rank) return true;
         if(this.isInGroup(this.questionRanks, liveCard.rank)){
             return ((attempt.suit === liveCard.suit) && !this.isInGroup(this.noneAnswers, attempt.rank));
         }
         if(continuousPlay) return false;
-        if(this.isInGroup(this.punisherRanks, liveCard.rank)){
-            return this.isInGroup(this.masterRanks, attempt.rank);
-        }
+        if(this.isInGroup(this.punisherRanks, liveCard.rank) && this.isInGroup(this.masterRanks, attempt.rank)) return true;
+        if(punish) return false;
         return attempt.suit === liveCard.suit;
     }
 }
@@ -74,9 +71,10 @@ class Game{
         this.min = min,
         this.players = [],
         this.cards = [],
-        this.currentPLayer = {
+        this.currentPlayer = {
             index : 0,
             picks : 1,
+            isFree: false,
             isMaster: false,
             playRound: 0
         },
@@ -118,66 +116,73 @@ class Game{
         for(let i = 0; i < 4; i++){
             this.players.forEach(player => this.issueCardToPlayer(player))
         }
-        this.currentPLayer.index = 0;
+        this.currentPlayer.index = 0;
         this.live = true;
         return this;
     }
     sendMessage(message){
         //to
+        //cardOnTAble
+        //cardi
+        //masterRequested
         //card matched
         //points earned
         //continuous play reward
     };
     goToNext(){
-        while(this.currentPLayer.picks > 0){
-            this.issueCardToPlayer(this.players[this.currentPLayer.index])
-            console.log("issue" + this.currentPLayer.picks);
-            this.currentPLayer.picks--;
+        if(!this.currentPlayer.isFree){
+            while(this.currentPlayer.picks > 0){
+                this.issueCardToPlayer(this.players[this.currentPlayer.index]);
+                console.log("issue" + this.currentPlayer.picks);
+                this.currentPlayer.picks--;
+            }
+            this.currentPlayer.picks = 1;
         }
-        let nextPLayerIndex = (this.currentPLayer.index + this.route) + (this.skip * this.route);
-        while(nextPLayerIndex > this.players.length - 1){
-            nextPLayerIndex -= this.players.length;
+        let nextPlayerIndex = (this.currentPlayer.index + this.route) + (this.skip * this.route);
+        while(nextPlayerIndex > this.players.length - 1){
+            nextPlayerIndex -= this.players.length;
         }
-        while(nextPLayerIndex < 0){
-            nextPLayerIndex += this.players.length;
+        while(nextPlayerIndex < 0){
+            nextPlayerIndex += this.players.length;
         }
         this.skip = 0;
         this.round += 1;
-        this.currentPLayer.playRound = 0;
-        this.currentPLayer.picks = 1;
-        this.currentPLayer.isMaster = false;
-        this.currentPLayer.index = nextPLayerIndex;
-        return nextPLayerIndex;
+        this.currentPlayer.playRound = 0;
+        this.currentPlayer.isMaster = false;
+        this.currentPlayer.isFree = false;
+        this.currentPlayer.index = nextPlayerIndex;
+        return nextPlayerIndex;
     }
     canPlay(playerId){
-        return this.live && (playerId === this.players[this.currentPLayer.index]);
+        return this.live && (playerId === this.players[this.currentPlayer.index].id);
     }
     callCardi(playerId){
         if(!this.canPlay(playerId)) return this;
-        this.players[this.currentPLayer.index].isCardi = true;
+        this.players[this.currentPlayer.index].isCardi = true;
         this.goToNext();
         return this;
     }
     mastersRequest(playerId, attempt){
         if(!this.canPlay(playerId)) return this;
-        if(!this.currentPLayer.isMaster) return this;
+        if(!this.currentPlayer.isMaster) return this;
         this.requestedSuit = attempt.suit;
         this.goToNext();
         return this;
     }
-    setPLay(attempt, liveCard){
-        this.players[this.currentPLayer.index].isCardi = false;
-        this.currentPLayer.picks = 0;
-        this.currentPLayer.playRound += 1;
+    setPlay(attempt, liveCard){
+        this.players[this.currentPlayer.index].isCardi = false;
+        this.currentPlayer.isFree = true;
+        this.currentPlayer.playRound += 1;
         this.requestedSuit = "";
-        if(!this.cards.isInGroup(this.cards.noneStarters, attempt.rank) && this.players[this.currentPLayer.index].cards.length < 1 && this.players[this.currentPLayer.index].isCardi){
+        if(!this.cards.isInGroup(this.cards.noneStarters, attempt.rank) && this.players[this.currentPlayer.index].cards.length < 1 && this.players[this.currentPlayer.index].isCardi){
             return 100;
         }
         if(this.cards.isInGroup(this.cards.masterRanks, attempt.rank)){
             if (this.cards.isInGroup(this.cards.punisherRanks, liveCard.rank)){
+                this.currentPlayer.picks = 1;
                 return 7;
             }
-            this.currentPLayer.isMaster = true;
+            this.currentPlayer.isMaster = true;
             return 13;
         }
         if(this.cards.isInGroup(this.cards.skippingRanks, attempt.rank)){
@@ -189,11 +194,11 @@ class Game{
             return 10;
         }
         if(this.cards.isInGroup(this.cards.questionRanks, attempt.rank)){
-            this.currentPLayer.picks = 1;
+            this.currentPlayer.isFree = false;
             return 10;
         }
         if(this.cards.isInGroup(this.cards.punisherRanks, attempt.rank)){
-            this.currentPLayer.picks += parseInt(attempt.rank);
+            this.currentPlayer.picks += parseInt(attempt.rank);
             return 3;
         }
         return 7;
@@ -202,8 +207,9 @@ class Game{
         if(!this.canPlay(playerId)) return this;
 
         let liveCard = this.cards.table[this.cards.table.length - 1];
-        let continuousPlay = this.currentPLayer.playRound > 0;
-        let continuousPlayReward = this.currentPLayer.playRound * 2;
+        let continuousPlay = this.currentPlayer.playRound > 0;
+        let continuousPlayReward = this.currentPlayer.playRound * 2;
+        let punish = this.currentPlayer.picks > 1;
 
         if(this.requestedSuit.length > 0){
             liveCard = {
@@ -215,13 +221,13 @@ class Game{
             this.goToNext();
             return this;
         }
-        let attemptCard = this.players[this.currentPLayer.index].cards[attempt.index];
-        if(!this.cards.match(attemptCard, liveCard, continuousPlay)) {
+        let attemptCard = this.players[this.currentPlayer.index].cards[attempt.index];
+        if(!this.cards.match(attemptCard, liveCard, continuousPlay, punish)) {
             return this
         };
-        let reward = this.setPLay(attemptCard, liveCard);
-        this.acceptCardFromPlayer(this.players[this.currentPLayer.index], attempt.index);
-        this.players[this.currentPLayer.index].points += (reward + continuousPlayReward);
+        let reward = this.setPlay(attemptCard, liveCard);
+        this.acceptCardFromPlayer(this.players[this.currentPlayer.index], attempt.index);
+        this.players[this.currentPlayer.index].points += (reward + continuousPlayReward);
         if(reward === 100){
             this.live = false;
             return this;
